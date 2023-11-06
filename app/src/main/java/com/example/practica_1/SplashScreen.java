443b55f5e36bc7aa833ba9ff5c941b8d9d9a6f11
Alejandro <tio.manco.2212@gmail.com>
2023-11-05 21:54:46 -0700

    Actu de disenho

---
+++ app/src/main/java/com/example/practica_1/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.practica_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    LottieAnimationView lottie;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        lottie = findViewById(R.id.lottie);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        },3000);
+    }
+}
